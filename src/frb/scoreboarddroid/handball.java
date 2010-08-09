67611afdd641efef9d45f77a2e5b721d0ec93a3b
fribell <fribell@dcb9d2e3-7f5e-e84c-0f56-09b644a55bb4>
2010-08-09 20:05:27 +0000

    Añadido menu y empezando deporte balonmano

---
+++ src/frb/scoreboarddroid/handball.java
@@ -0,0 +1,16 @@
+package frb.scoreboarddroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class handball extends Activity {
+		
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.handballmatch);
+    }
+    
+}
--- src/frb/scoreboarddroid/main.java
+++ src/frb/scoreboarddroid/main.java
@@ -1,13 +1,47 @@
 package frb.scoreboarddroid;
 
+import java.lang.reflect.Array;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class main extends Activity {
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    public void startMatch(View v){
+    	
+    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    	builder.setTitle(R.string.select_sport);
+    	builder.setItems(R.array.esports,new DialogInterface.OnClickListener() {
+    	    public void onClick(DialogInterface dialog, int item) {
+    	    	String[] items = getResources().getStringArray(R.array.esports);
+
+    	        if( items[item].equalsIgnoreCase("balonmano") ){
+	            	Intent new_intent = new Intent(frb.scoreboarddroid.main.this, handball.class);
+	                new_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	                startActivity(new_intent);	     
+	                finish();
+    	        }
+    	        
+    	    }
+    	});
+    	AlertDialog alert = builder.create();
+    	alert.show();    	
+    }
+    
+    public void exit(View v){
+        finish();
+    }
+    
 }
