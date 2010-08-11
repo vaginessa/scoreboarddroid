be57dd558f6caeec9523579998813bb84efa56b3
fribell <fribell@dcb9d2e3-7f5e-e84c-0f56-09b644a55bb4>
2010-08-11 06:57:16 +0000

    puntuacions ja funcionen

--- src/frb/scoreboarddroid/handball.java
+++ src/frb/scoreboarddroid/handball.java
@@ -5,6 +5,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Chronometer;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -56,6 +57,43 @@ public class handball extends Activity {
         	}
         });
         
+        //evento click de los botones
+        ImageButton but_addLocal = (ImageButton) this.findViewById(R.id.but_addLocal);
+        ImageButton but_removeLocal = (ImageButton) this.findViewById(R.id.but_removeLocal);
+        ImageButton but_addVisitor = (ImageButton) this.findViewById(R.id.but_addVisitor);
+        ImageButton but_removeVisitor = (ImageButton) this.findViewById(R.id.but_removeVisitor);
+        but_addLocal.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		TextView  points = (TextView) findViewById(R.id.point_local);
+        		changePoints(points,"+");
+        	}
+        });
+        
+        but_removeLocal.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		TextView  points = (TextView) findViewById(R.id.point_local);
+        		changePoints(points,"-");
+        	}
+        });
+        
+        but_addVisitor.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		TextView  points = (TextView) findViewById(R.id.point_visitor);
+        		changePoints(points,"+");
+        	}
+        });
+        
+        but_removeVisitor.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		TextView  points = (TextView) findViewById(R.id.point_visitor);
+        		changePoints(points,"-");
+        	}
+        });
+        
     }
     
     public void startStopMatch(View v){
@@ -75,15 +113,14 @@ public class handball extends Activity {
     	}
     }
     
-    public void addPoint(View v){
-    	    	
-    	//ImageButton but = (ImageButton) this.findViewById(v.getId());
+    public void changePoints(TextView points, String op){
+    	Integer result = 0;
+    	if( op.equalsIgnoreCase("+"))
+    		result = Integer.parseInt(points.getText().toString())+1;
+    	else if(!points.getText().toString().equalsIgnoreCase("0"))
+			result = Integer.parseInt(points.getText().toString())-1;
     	
-		TextView  points = (TextView) this.findViewById(R.id.point_local);
-		Integer suma = Integer.parseInt(points.getText().toString())+1;
-		points.setText(suma);
-    	
-    	
+		points.setText(result.toString());
     	
     }
     
